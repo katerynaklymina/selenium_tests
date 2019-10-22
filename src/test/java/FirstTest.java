30b391b9cdcc634fc99cb2090fc83843328483e3
Kateryna Klymina <kateryna.klymina@synapse.com>
2019-10-23 00:41:49 +0300

    added working test

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,8 +1,11 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class FirstTest {
     private WebDriver driver;
@@ -14,7 +17,14 @@ public class FirstTest {
 
     @Before
     public void openBrowser() {
-        driver = new ChromeDriver();
+
+        //DesiredCapabilities caps = new DesiredCapabilities();
+       // caps.setCapability("unexpectedAlertBehaviour", "accept");
+
+        ChromeOptions ch = new ChromeOptions();
+        ch.addArguments("start-maximized");
+
+        driver = new ChromeDriver(ch);
     }
 
     @After
@@ -25,11 +35,16 @@ public class FirstTest {
     @Test
     public void googleTest() {
         driver.get("http://google.com");
+        System.out.println(((HasCapabilities) driver).getCapabilities());
     }
 
     @Test
     public void ukrNetTest() {
+
         driver.get("http://ukr.net");
+
+        System.out.println(((HasCapabilities) driver).getCapabilities());
     }
+
 }
 
