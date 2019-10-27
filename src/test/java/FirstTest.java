ba01c5fc2678d66f0c7588005c7c3868021ea36b
kateryna.klymina <kateryna.klymina@globallogic.com>
2019-10-28 00:47:25 +0200

    Added tests for firefox and internet explorer, tried to implement waits fro firefox

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -9,10 +9,15 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class FirstTest {
     private WebDriver driver;
-    public static final String PATH_CHROME_DRIVER = "C:\\ChromeDriver\\chromedriver.exe";
+    public static final String PATH_MOZ_DRIVER = "D:\\projects\\selenium\\drivers\\geckodriver.exe";
+    public static final String PATH_CHROME_DRIVER = "D:\\projects\\selenium\\drivers\\chromedriver.exe";
+    public static final String PATH_MSEDGE_DRIVER = "D:\\projects\\selenium\\drivers\\msedgedriver.exe";
 
     public FirstTest() {
+
         System.setProperty("webdriver.chrome.driver", FirstTest.PATH_CHROME_DRIVER);
+        System.setProperty("webdriver.gecko.driver", FirstTest.PATH_MOZ_DRIVER);
+        System.setProperty("webdriver.msedge.driver", FirstTest.PATH_MSEDGE_DRIVER);
     }
 
     @Before
